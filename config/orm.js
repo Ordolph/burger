7aa3ec5cd7e3645449003373c9a7b47271febcdd
Ordolph <austin.t.randolph@gmail.com>
2019-01-06 14:20:05 -0500

    Added basic mysql queries to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,34 @@
 require('./connection.js');
+
+module.exports = function (orm) {
+    function selectAll(table) {
+        connection.query('SELECT * FROM ?', [table], function (err, res) {
+            if (err) {
+                throw err
+            };
+
+            return res;
+        }
+        );
+    };
+
+    function insertOne(table, col, val) {
+        connection.query('INSERT INTO ? (?) VALUES ?', [table, col, val], function (err, res) {
+            if (err) {
+                throw err
+            };
+
+            return res;
+        });
+    };
+
+    function updateOne(table, col, val, id) {
+        connection.query('UPDATE ? SET ? = ?, WHERE id = ?', [table, col, val, id], function (err, res) {
+            if (err) {
+                throw err;
+            };
+
+            return res;
+        });
+    };
+}
