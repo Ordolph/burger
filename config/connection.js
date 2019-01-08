1dc141a5b41e5e8bd8a8f56b0bd09176df291c5f
Ordolph <austin.t.randolph@gmail.com>
2019-01-07 23:16:16 -0500

    Hooked connection to JAWSDB for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASS,
-    port: 3306,
-    database: 'burgers_db'
-});
+var connection;
+
+if (process.nextTick.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
+else {
+    connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.PASS,
+        port: 3306,
+        database: 'burgers_db'
+    });
+}
+
 
 connection.connect(function (err) {
     if (err) {
